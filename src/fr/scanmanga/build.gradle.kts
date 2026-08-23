2e6a18136d1b1826ebe04c24a3b659324f73181a
anderson76389 <jhtefhjlo@proton.me>
2026-08-23 23:45:25 +0200

    Update build.gradle.kts

--- src/fr/scanmanga/build.gradle.kts
+++ src/fr/scanmanga/build.gradle.kts
@@ -1,13 +1,9 @@
-plugins {
-    id("com.android.application")
-}
-
 ext {
-    extName = "Scan-Manga"
-    pkgNameSuffix = "fr.scanmanga"
-    extClass = ".ScanManga"
-    extVersionCode = 1
-    libVersion = "1.4"
+    set("extName", "Scan-Manga")
+    set("pkgNameSuffix", "fr.scanmanga")
+    set("extClass", ".ScanManga")
+    set("extVersionCode", 1)
+    set("libVersion", "1.4")
 }
 
 apply(from = "$rootDir/common.gradle")
